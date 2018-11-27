10e8a7ff781bfb1f0700146119b11c3638a0f64e
Shanya246 <45380630+Shanya246@users.noreply.github.com>
2018-11-27 11:59:50 +0530

    Create exercise1_1.c

---
+++ exercise1_1.c
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+int main()
+{
+printf("\n Hello world ");
+}
+
+
+
+
+Warning message shows that : imcompatible implicit declaration of built in function 'printf'
+
+
+OUTPUT :
+
+Hello world
